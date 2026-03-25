f93052cf316de0afa56ddfd57a22ff9e1f514f5f
Santiago Alvarez <santiiairel2004@gmail.com>
2026-03-25 19:05:47 -0300

    Implementacion de Punto 2

---
+++ src/tp1/punto2/Contador.java
@@ -0,0 +1,42 @@
+package tp1.punto2;
+
+import java.util.Scanner;
+
+public class Contador {
+	
+	public static int [] NumerosMultiplos (int num) {
+		int [] listaNumeros = new int [num];
+		
+		for (int i = 1; i <= num; i++) {
+			listaNumeros[i - 1] = i * num;
+		}
+		
+		return listaNumeros;
+		
+	}
+	
+	public static void main(String[] args) {
+			
+			Contador contador = new Contador();
+			Scanner scan = new Scanner (System.in);
+			
+			System.out.print("ingrese para obtener una lista de numeros multiples de la misma longitud");
+			System.out.print("se corta con -1 ");
+			
+			int num = scan.nextInt();
+			
+			while ( num != -1 ) {
+				int [] lista = new int [num];
+				
+				lista = contador.NumerosMultiplos(num);
+				
+				for (int i : lista) {
+					System.out.print("\n" + i);
+				}
+				
+				System.out.print("\n Ingrese otro numero");
+				num = scan.nextInt();
+			}
+		}
+	
+}
